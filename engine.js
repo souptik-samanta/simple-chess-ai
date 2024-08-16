dd3d5174a9f271149292ccae44cd051a6f85d360
Souptik Samanta <161435060+souptik-samanta@users.noreply.github.com>
2024-08-17 00:08:36 +0530

    Add files via upload

--- engine.js
+++ engine.js
@@ -1,5 +1,4 @@
 const chess = require("chess.js");
-//souptik
 const game = new chess.Chess('6k1/5ppp/8/8/8/8/6P1/4Q1K1 w - - 0 1');
 
 // using chess.js to make things simpler :)
@@ -35,3 +34,50 @@ evaluateBoard('rnb1kbnr/ppp1pppp/8/8/2pN4/8/PP2PPPP/RNBQKB1R b KQkq - 0 4');
    -negamax(params) determines the current state of the position -- maximising or minimizing
    adjust your depth nodes to get more search trees, however, your search time would be significantly increased
 */
+
+function negamax(depth, color) { // thanks to chessprogramming.wiki and chatgpt-4 for helping me out with this algorithm
+    if (game.isGameOver() || depth === 0) { 
+        return 0;
+    }
+
+    let maxEval = Number.NEGATIVE_INFINITY; // setting the maximum evaluation to negative infinity
+
+    let samples = game.moves({ verbose: true });
+    let moves = samples.map(sample => sample.lan); // getting each move in the UCI format with the lan child
+
+    for (let move of moves) { // iterating through each lan move
+        game.move(move);     // making the move on the board
+        let eval = -negamax(depth - 1, -color);     /* calling the negamax function with a negative and pushing a -color param to automatically revert values
+                                                       like if it is white's turn, -negamax(depth -1, -1) since -1 is the opposite of initially given param 1
+                                                       for black, its just the reverse. */
+        game.undo();         // returning to the initial position on the board
+
+        maxEval = Math.max(maxEval, eval);   // finding the maximum evaluation
+    }
+
+    return maxEval;
+}
+
+function makeBestMove(depth) { // making the best move
+    let bestMove = null;
+    let maxEval = Number.NEGATIVE_INFINITY;
+
+    let samples = game.moves({ verbose: true });
+    let moves = samples.map(sample => sample.lan);
+
+    for (let move of moves) {
+        game.move(move);
+        let eval = -negamax(depth - 1, -1);   // calling negamax
+        game.undo();
+
+        if (eval > maxEval) {
+            maxEval = eval;
+            bestMove = move;
+        }
+    }
+
+    return bestMove;
+}
+
+const bestMove = makeBestMove(2); // evaluating the best move at depth 2
+console.log(bestMove) //your best Move
